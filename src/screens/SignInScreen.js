2af45e62aa795a47927b6a026d6fdb9eb28a4232
Sergio Valencia <serg337@gmail.com>
2019-12-23 14:45:48 +0100

    Initial Commit - All screens created

---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SignInScreen = () => {
+  return (
+    <View>
+      <Text>Sign In Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SignInScreen;
