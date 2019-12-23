a97f09d6422997fd64dd0f9afd337f9300cf7ee5
Sergio Valencia <serg337@gmail.com>
2019-12-23 16:22:14 +0100

    Implemented Sign Up form in Screen

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,22 +1,50 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Input, Button, Text } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
 const SignUpScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View>
-      <Text>Sign Up Screen</Text>
-      <Button
-        title='Go to Sign In'
-        onPress={() => navigation.navigate("SignIn")}
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input
+        label='Email'
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize='none'
+        autoCorrect={false}
       />
-      <Button
-        title='Go to Main Flow'
-        onPress={() => navigation.navigate("mainFlow")}
+      <Spacer />
+      <Input
+        label='Password'
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize='none'
+        autoCorrect={false}
       />
+      <Spacer>
+        <Button title='Sign Up' />
+      </Spacer>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignUpScreen.navigationOptions = () => {
+  return {
+    header: null
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.7,
+    justifyContent: "center"
+  }
+});
 
 export default SignUpScreen;
