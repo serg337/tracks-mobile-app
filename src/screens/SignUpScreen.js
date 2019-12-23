9f3c9c81a466133fc59b24c0d16a98a308662e8f
Sergio Valencia <serg337@gmail.com>
2019-12-23 15:10:07 +0100

    Implemented Stack, Switch and Bottom Tab navigators - Basic

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-const SignUpScreen = () => {
+const SignUpScreen = ({ navigation }) => {
   return (
     <View>
       <Text>Sign Up Screen</Text>
+      <Button
+        title='Go to Sign In'
+        onPress={() => navigation.navigate("SignIn")}
+      />
+      <Button
+        title='Go to Main Flow'
+        onPress={() => navigation.navigate("mainFlow")}
+      />
     </View>
   );
 };
